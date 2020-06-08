40241761d044c4cbd20d852f5fc3d7ab23774772
Dev Vashishta <dev.vashishta@nagarro.com>
2020-06-08 13:25:08 +0530

    end to end test case added

--- e2e/todo-spec.js
+++ e2e/todo-spec.js
@@ -1,5 +1,3 @@
-import { browser, element, by } from "protractor";
-import { Alert } from 'selenium-webdriver';
 describe('Protractor LoginPage App', function() {
   it('should redirect to logiSuccess page if correct user name & password is entered', function() {
     browser.get('http://localhost:4200/loginPage');
@@ -18,17 +16,11 @@ describe('Protractor LoginPage App', function() {
     element(by.id('userName')).sendKeys("Admin");
     element(by.id('password')).sendKeys("wrongPassword");
     element(by.id('submitButton')).click();
-      browser.sleep(2000);
-		let ale:Alert = browser.switchTo().alert();
-		// clicks 'OK' button
-		ale.accept();
-
     browser.sleep(2000).then(function(){
     browser.getCurrentUrl().then(function(actualUrl){ // promise
       console.log("Actual url",actualUrl);
       expect(actualUrl.indexOf('loginPage') !== -1).toBeTruthy(); // check the current url is list
     });
   });
-
   });
 });
--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -15,11 +15,11 @@ import { Router } from '@angular/router';
 export class LoginPageComponent implements OnInit {
   loginForm = new FormGroup({
     userName: new FormControl('', {
-      updateOn: 'blur',
+      updateOn: 'change',
       validators: [Validators.required, Validators.minLength(5)],
     }),
     password: new FormControl('', {
-      updateOn: 'blur',
+      updateOn: 'change',
       validators: [Validators.required, Validators.minLength(5)],
     }),
   });
@@ -28,15 +28,17 @@ export class LoginPageComponent implements OnInit {
 
   ngOnInit(): void {}
   onSubmit() {
-    console.log("Routed to 1login success");
     if (
       this.loginForm.controls.userName.value === 'Admin' &&
       this.loginForm.controls.password.value === 'password'
     ) {
       this.router.navigate(['/loginSuccess']);
-      console.log("Routed to login success");
     } else {
       alert('Incorrect User name or password,try again');
     }
   }
 }
+
+
+
+
